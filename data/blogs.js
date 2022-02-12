ca95c52490d8a29629e448dbbf0ae06f7ec2e1b9
APURV CHOUBEY <apurv.main2@gmail.com>
2022-02-12 11:36:04 +0530

    Added Routes and templates

---
+++ data/blogs.js
@@ -0,0 +1,26 @@
+// Creating an array of objects as blogs then exporting it
+blogs = [
+    {
+        title: "How to get started with HTML",
+        content: "HTML contents to start",
+        slug: "HTML-basics"
+    },
+    {
+        title: "How to get started with CSS",
+        content: "CSS contents to start",
+        slug: "CSS-basics"
+    },
+    {
+        title: "How to get started with Javascript",
+        content: "Javascript contents to start",
+        slug: "Javascript-basics"
+    },
+    {
+        title: "How to get started with Node JS",
+        content: "Node JS contents to start",
+        slug: "Node-JS-basics"
+    },
+]
+
+
+module.exports = blogs;
